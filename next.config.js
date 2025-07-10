41055edd1d345f27ce5d9737580385456befdc71
CodiX 🧑‍💻 <codix810@gmail.com>
2025-07-10 13:44:10 +0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: {}
-  }
-}
+    serverActions: true, // ✅ لازم تبقى جوه object
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
