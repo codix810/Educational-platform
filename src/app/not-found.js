053981e17d4a0abdf6858c716465c8237399a542
CodiX 🧑‍💻 <codix810@gmail.com>
2025-07-10 12:40:53 +0300

    not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,4 +1,6 @@
 // ✅ المسار: src/app/not-found.js
+// @ts-nocheck
+
 export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center">
