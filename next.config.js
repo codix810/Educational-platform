accc4c60acf88d138ca239047fed7ae3287c339a
CodiX 🧑‍💻 <codix810@gmail.com>
2025-07-20 13:16:45 +0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: {}, // ✅ كده تمام
   },
   eslint: {
-    ignoreDuringBuilds: true, // ✅ ده يمنع Vercel يوقف الـ build بسبب ESLint
+    ignoreDuringBuilds: true,
   },
 };
 
